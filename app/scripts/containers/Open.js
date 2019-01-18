08980bbe0773fd0ee3014bc3572eb5439b3318eb
antelle <dmitryvit@gmail.com>
2019-01-18 21:47:08 +0100

    open

--- app/scripts/containers/Open.js
+++ app/scripts/containers/Open.js
@@ -58,6 +58,7 @@ const mapDispatchToProps = dispatch => {
         },
         onFileDeleteClick({ id }) {
             // TODO: question about in-memory files
+            // TODO: question about modified storage files
             dispatch(removeLastFile(id));
         },
     };
