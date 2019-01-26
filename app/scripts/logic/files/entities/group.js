46e010d95db5ec314d36ff2e11444836a054cf65
antelle <dmitryvit@gmail.com>
2019-01-26 10:46:23 +0100

    icons

--- app/scripts/logic/files/entities/group.js
+++ app/scripts/logic/files/entities/group.js
@@ -1,3 +1,10 @@
+import kdbxweb from 'kdbxweb';
+import { IconMap } from 'const/icon-map';
+import { toDataUrl } from 'util/text/icon-url';
+
+const KdbxIcons = kdbxweb.Consts.Icons;
+const ignoredIcons = [KdbxIcons.Folder, KdbxIcons.FolderOpen];
+
 export function groupToModel(kdbx, kdbxGroup, file, parentUuid, nestingLevel) {
     const isRecycleBin = kdbxGroup.uuid.equals(kdbx.meta.recycleBinUuid);
     return {
@@ -18,6 +25,19 @@ export function groupToModel(kdbx, kdbxGroup, file, parentUuid, nestingLevel) {
         autoTypeSeq: kdbxGroup.defaultAutoTypeSeq,
         title: parentUuid ? kdbxGroup.name : file.name,
         iconId: kdbxGroup.icon,
+        icon: iconFromId(kdbxGroup.icon),
         customIconId: kdbxGroup.customIcon ? kdbxGroup.customIcon.toString() : null,
+        customIcon: buildCustomIcon(kdbxGroup.customIcon, kdbx),
     };
+}
+
+function iconFromId(id) {
+    if (ignoredIcons.includes(id)) {
+        return undefined;
+    }
+    return IconMap[id];
+}
+
+function buildCustomIcon(customIcon, kdbx) {
+    return customIcon ? toDataUrl(kdbx.meta.customIcons[customIcon]) : null;
 }
