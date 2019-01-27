a2d65503ff02cdf230973776e693e62b6c454e43
antelle <dmitryvit@gmail.com>
2019-01-27 18:21:03 +0100

    empty details

--- app/scripts/components/Details.js
+++ app/scripts/components/Details.js
@@ -8,21 +8,24 @@ class Details extends React.Component {
     };
     render() {
         const { entry } = this.props;
-        const { title } = entry || {};
+        if (!entry) {
+            return this.renderEmpty();
+        }
+        const { title } = entry;
         return (
             <div className="details">
-                {!entry && (
-                    <div className="empty-block muted-color">
-                        <h1 className="empty-block__title">
-                            <Res id="detEmpty" />
-                        </h1>
-                    </div>
-                )}
-                {entry && (
-                    <div className="details__header">
-                        <h1 className="details__header-title">{title}</h1>
-                    </div>
-                )}
+                <div className="details__header">
+                    <h1 className="details__header-title">{title}</h1>
+                </div>
+            </div>
+        );
+    }
+    renderEmpty() {
+        return (
+            <div className="empty-block muted-color">
+                <h1 className="empty-block__title">
+                    <Res id="detEmpty" />
+                </h1>
             </div>
         );
     }
