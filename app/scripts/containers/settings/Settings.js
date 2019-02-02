0752e3149d9367650055d67c86f8fa4b2fc41878
antelle <dmitryvit@gmail.com>
2019-02-02 18:39:40 +0100

    settings

--- app/scripts/containers/settings/Settings.js
+++ app/scripts/containers/settings/Settings.js
@@ -1,24 +1,38 @@
 import { connect } from 'react-redux';
 import { Settings } from 'components/settings/Settings';
 import { setView } from 'store/ui/set-view';
+import { getActiveFiles } from 'selectors/files';
 
 const mapStateToProps = state => {
     return {
+        files: getActiveFiles(state),
         page: state.menu.settings.active,
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
+        setView(view) {
+            dispatch(setView(view));
+        },
+    };
+};
+
+const mergeProps = (stateProps, dispatchProps, ownProps) => {
+    return {
+        ...ownProps,
+        ...stateProps,
+        ...dispatchProps,
         onReturnClick() {
-            dispatch(setView('list'));
+            dispatchProps.setView(stateProps.files.length ? 'list' : 'open');
         },
     };
 };
 
 const SettingsContainer = connect(
     mapStateToProps,
-    mapDispatchToProps
+    mapDispatchToProps,
+    mergeProps
 )(Settings);
 
 export { SettingsContainer as Settings };
