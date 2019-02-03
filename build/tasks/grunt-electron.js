e05c2426ec0d00bbcef8c22596f5c8f4cbb330f6
antelle <dmitryvit@gmail.com>
2019-02-03 14:51:53 +0100

    build

---
+++ build/tasks/grunt-electron.js
@@ -0,0 +1,14 @@
+const electronPackager = require('electron-packager');
+
+module.exports = grunt => {
+    grunt.registerMultiTask('electron', 'Package Electron apps', function() {
+        const done = this.async();
+
+        electronPackager(this.options())
+            .then(() => done())
+            .catch(err => {
+                grunt.warn(err);
+                done();
+            });
+    });
+};
